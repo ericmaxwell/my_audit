ff5ae6cdce2427c81d1b7b8d004e0ba02420d90a
iamericmaxwell <iamericmaxwell@gmail.com>
2017-02-15 17:02:06 -0800

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,19 @@
+name 'my_audit'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures my_audit'
+long_description 'Installs/Configures my_audit'
+version '0.1.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/my_audit/issues' if respond_to?(:issues_url)
+
+# The `source_url` points to the development reposiory for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/my_audit' if respond_to?(:source_url)
