f719ebb5431e85a5c7ee79943cd9c750505ed556
iamericmaxwell <iamericmaxwell@gmail.com>
2017-03-24 16:38:24 -0700

    Changed defaults

--- metadata.rb
+++ metadata.rb
@@ -1,19 +1,9 @@
 name 'my_audit'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'iamericmaxwell'
+maintainer_email 'emaxwell@chef.io'
 license 'all_rights'
-description 'Installs/Configures my_audit'
+description 'Wrapper cookbook for Audit'
 long_description 'Installs/Configures my_audit'
-version '0.1.0'
+version '0.1.2'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/my_audit/issues' if respond_to?(:issues_url)
-
-# The `source_url` points to the development reposiory for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/my_audit' if respond_to?(:source_url)
+depends 'audit'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,21 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+node.default['audit']['inspec_version'] = 'latest'
+
+#Defines the collector or collectors. Can take single item or items[]
+#This specific config will push data directly to an existing Compliance server
+node.default['audit']['collector'] = 'chef-compliance' 
+node.default['audit']['server'] = '[SERVER URL]/api'
+node.default['audit']['owner'] = '[ORG NAME]'
+node.default['audit']['refresh_token'] = '[REFRESH TOOKEN]'
+node.default['audit']['insecure'] = false
+node.default['audit']['profiles'] = [
+    {
+      "name": "linux",
+      "compliance": "base/linux"
+    }
+  ]
+
+include_recipe 'audit::default'
